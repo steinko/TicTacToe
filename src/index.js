b250c28b864a62e82e94432a891eef3dc8dca09d
stein_korsveien@msn.com <stein_korsveien>
2018-02-25 16:53:44 +0100

    edit index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Game from './Game';
-ReactDOM.render(<Game />, document.getElementById('root'));
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Game from './Game'
+ReactDOM.render(<Game />, document.getElementById('root'))
