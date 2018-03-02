8bc664ee708065bd14a4ea4386443a29886fecdf
stein_korsveien@msn.com <stein_korsveien>
2018-03-02 16:18:53 +0100

    added eslintrc.json and correnect esslint errors

--- src/Board.jsx
+++ src/Board.jsx
@@ -21,13 +21,13 @@ export default class Board extends React.Component {
   }
     
   render () { 
-      const winner = calculateWinner(this.state.squares)
-      let status
-      if (winner) {
-        status = 'Winner: ' + winner
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-      }
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div> 
@@ -54,15 +54,13 @@ export default class Board extends React.Component {
   handleClick (i) { 
     const squares = this.state.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
-      return;
+      return
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
-      xIsNext: !this.state.xIsNext,
+      xIsNext: !this.state.xIsNext
     })
   }
   
 }
-
-
--- test/Board.test.jsx
+++ test/Board.test.jsx
@@ -33,9 +33,9 @@ describe('A board', () => {
     expect(board.find('div.status').text()).toBe('Next player: X ')     
   })
   
- xit('should contain: Next player: = 0 ', () => {
+  xit('should contain: Next player: = 0 ', () => {
     boardInst.handleClick(1)
-     board = shallow(<Board />)
+    board = shallow(<Board />)
     expect(board.find('div.status').text()).toBe('Next player: O ')     
   })
       
@@ -70,44 +70,44 @@ describe('A board', () => {
   
   it('should change xIsNext to false', () => { 
     boardInst.handleClick(1)
-    expect(board.state().xIsNext).toBeFalsy
+    expect(board.state().xIsNext).toBeFalsy()
   })
   
-  it('should be a winner X' , () => { 
-      var squares =  ['X','X','X','O','O',null,null,null,null,null,null,null,]
-      const winner = calculateWinner(squares)
-      expect(winner).toBe('X')
+  it('should be a winner X', () => { 
+    var squares = ['X', 'X', 'X', 'O', 'O', null, null, null, null, null, null, null]
+    const winner = calculateWinner(squares)
+    expect(winner).toBe('X')
   })
   
-   it('should be a winner O' , () => { 
-      var squares =  ['O','O','O','X','X',null,null,null,null,null,null,null,]
-      const winner = calculateWinner(squares)
-      expect(winner).toBe('O')
+  it('should be a winner O', () => { 
+    var squares = ['O', 'O', 'O', 'X', ' X', null, null, null, null, null, null, null]
+    const winner = calculateWinner(squares)
+    expect(winner).toBe('O')
   })
   
-  it('should not be a winner' , () => { 
-      var squares =  ['O','X','O','X','O',null,null,null,null,null,null,null,]
-      const winner = calculateWinner(squares)
-      expect(winner).toBe(null)
+  it('should not be a winner', () => { 
+    var squares = ['O', 'X', 'O', 'X', 'O', null, null, null, null, null, null, null]
+    const winner = calculateWinner(squares)
+    expect(winner).toBe(null)
   })
   
-  xit('shold be a winner X' , () => { 
-      boardInst.renderSquare(0)
-      boardInst.renderSquare(3)
-      boardInst.renderSquare(1)
-      boardInst.renderSquare(4)
-      boardInst.renderSquare(2)
-      expect(board.find('div.status').text()).toBe('Winner: X')
+  xit('shold be a winner X', () => { 
+    boardInst.renderSquare(0)
+    boardInst.renderSquare(3)
+    boardInst.renderSquare(1)
+    boardInst.renderSquare(4)
+    boardInst.renderSquare(2)
+    expect(board.find('div.status').text()).toBe('Winner: X')
   })
   
-  xit('shold be a winner X' , () => { 
-      boardInst.renderSquare(3)
-      boardInst.renderSquare(0)
-      boardInst.renderSquare(4)
-      boardInst.renderSquare(1)
-      boardInst.renderSquare(6)
-      boardInst.renderSquare(2)
-      expect(board.find('div.status').text()).toBe('Winner: O')
+  xit('shold be a winner X', () => { 
+    boardInst.renderSquare(3)
+    boardInst.renderSquare(0)
+    boardInst.renderSquare(4)
+    boardInst.renderSquare(1)
+    boardInst.renderSquare(6)
+    boardInst.renderSquare(2)
+    expect(board.find('div.status').text()).toBe('Winner: O')
   })
   
 })
